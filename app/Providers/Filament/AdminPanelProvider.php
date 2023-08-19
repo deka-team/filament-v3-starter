3aaa40dca2c937e8a7c3451307b057f77326c99e
riskihajar <riskihajar@gmail.com>
2023-08-19 11:59:17 +0800

    install role permission

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,6 +41,9 @@ class AdminPanelProvider extends PanelProvider
                     }
                 </style>
             HTML))
+            ->plugins([
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
