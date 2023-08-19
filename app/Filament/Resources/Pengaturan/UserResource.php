1ba7db26b3fbb5f6b58651f2f7060acea812cde6
riskihajar <riskihajar@gmail.com>
2023-08-19 12:45:14 +0800

    improve: roles + users

--- app/Filament/Resources/Pengaturan/UserResource.php
+++ app/Filament/Resources/Pengaturan/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
+    protected static ?string $slug = '/pengaturan/users';
     protected static ?string $navigationIcon = 'heroicon-o-user-group';
     protected static ?string $navigationGroup = 'Pengaturan';
 
--- config/filament-shield.php
+++ config/filament-shield.php
@@ -3,7 +3,7 @@
 return [
     'shield_resource' => [
         'should_register_navigation' => true,
-        'slug' => 'shield/roles',
+        'slug' => 'pengaturan/roles',
         'navigation_sort' => -1,
         'navigation_badge' => true,
         'navigation_group' => true,
@@ -69,7 +69,9 @@ return [
             'AccountWidget', 'FilamentInfoWidget',
         ],
 
-        'resources' => [],
+        'resources' => [
+
+        ],
     ],
 
     'register_role_policy' => [
