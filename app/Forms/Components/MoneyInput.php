530392f959d9fafd02fc829e6655df07b9631bc9
riskihajar <riskihajar@gmail.com>
2023-09-16 14:41:12 +0700

    add number input

--- app/Forms/Components/MoneyInput.php
+++ app/Forms/Components/MoneyInput.php
@@ -2,25 +2,13 @@
 
 namespace App\Forms\Components;
 
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Get;
-use Filament\Forms\Set;
-use Filament\Support\RawJs;
-
-class MoneyInput extends TextInput
+class MoneyInput extends NumberInput
 {
     protected function setUp(): void
     {
+        parent::setUp();
+
         $this->prefix('Rp ')
-            ->suffix(',-')
-            ->mask(RawJs::make(<<<JS
-                \$money(\$input)
-            JS))
-            ->formatStateUsing(fn(null|string $state) => number_format($state))
-            ->beforeStateDehydrated(function (Get $get, Set $set) {
-                $name = $this->getName();
-                $value = preg_replace('/[^0-9.]/', '', $get($name));
-                $set($name, $value);
-            });
+            ->suffix(',-');
     }
 }
---
+++ app/Forms/Components/NumberInput.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Forms\Components;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Support\RawJs;
+
+class NumberInput extends TextInput
+{
+    protected function setUp(): void
+    {
+        $this->mask(RawJs::make(<<<JS
+                \$money(\$input)
+            JS))
+            ->formatStateUsing(fn(null|string $state) => number_format($state))
+            ->beforeStateDehydrated(function (Get $get, Set $set) {
+                $name = $this->getName();
+                $value = preg_replace('/[^0-9.]/', '', $get($name));
+                $set($name, $value);
+            });
+    }
+}
