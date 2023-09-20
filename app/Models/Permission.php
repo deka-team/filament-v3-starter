cc97ad79661d71ee07474a8cd2dcba9d44111eff
RiloArbabillah <rilo.arbabillah@gmail.com>
2023-09-20 14:08:06 +0800

    change from custom concerns to default iluminate

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use App\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUlids as ConcernsHasUlids;
 
 class Permission extends \Spatie\Permission\Models\Permission
 {
-    use HasUlids;
+    use ConcernsHasUlids;
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use App\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUlids as ConcernsHasUlids;
 
 class Role extends \Spatie\Permission\Models\Role
 {
-    use HasUlids;
+    use ConcernsHasUlids;
 }
